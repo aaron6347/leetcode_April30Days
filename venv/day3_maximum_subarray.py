fe749505eb9fc495573be402b012113fe7621b40
aaron6347 <49943930+aaron6347@users.noreply.github.com>
2020-04-04 06:38:39 +0800

    aa

---
+++ venv/day3_maximum_subarray.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current=maxx=0
+        for x in range(len(nums)):
+            current+=nums[x]
+            if current > maxx:
+                maxx=current
+            if current<0:
+                current=0
+        return maxx
+
+run=Solution()
+a=[-2,1,-3,4,-1,2,1,-5,4]
+# a=[2,0,-3,2,1,0,1,-2]
+print(run.maxSubArray(a))
